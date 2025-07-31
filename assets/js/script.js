a81e39a375018c0160e0f41c4db37cba8ffdef23
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-07-31 14:42:34 +0100

    Add skeleton of game panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+let welcome;
+let instructions;
+let game;
+let playBtn;
+let instructionsBtn;
+let fiftyBtn;
+let askBtn;
+let phoneBtn;
+
 // Wait for the DOM to finish loading before running this script
 document.addEventListener('DOMContentLoaded', () => {
+    // set up DOM variables
+    welcome = document.querySelector('#welcome');
+    instructions = document.querySelector('#instructions');
+    game = document.querySelector('#game');
+    playBtn = document.querySelector('#play-btn');
+    instructionsBtn = document.querySelector('#instructions-btn');
+    fiftyBtn = document.querySelector('#fifty-btn');
+    askBtn = document.querySelector('#ask-btn');
+    phoneBtn = document.querySelector('#phone-btn');
 
-    // add event listeners to each button
-    const instructionsButton = document.querySelector('#instructions-btn')
-    instructionsButton.addEventListener('click', showInstructions);
+    // add event listeners to buttons
+    instructionsBtn.addEventListener('click', showInstructions);
+    playBtn.addEventListener('click', startGame);
 });
 
 
@@ -11,11 +29,24 @@ document.addEventListener('DOMContentLoaded', () => {
  * Show the instructions panel
  */
 function showInstructions() {
-    const instructions = document.querySelector('#instructions-txt');
-    const welcome = document.querySelector('#welcome-txt');
-    const instructionsButton = document.querySelector('#instructions-btn');
-
     instructions.classList.toggle('hidden');
     welcome.classList.toggle('hidden');
     instructionsButton.classList.toggle('hidden');
+}
+
+
+/**
+ * Show the game panel and start the game
+ */
+function startGame() {
+    // hide non-in-game content
+    welcome.classList.toggle('hidden');
+    playBtn.classList.toggle('hidden');
+    instructionsBtn.classList.toggle('hidden');
+    fiftyBtn.classList.toggle('hidden');
+    askBtn.classList.toggle('hidden');
+    phoneBtn.classList.toggle('hidden');
+
+    // show the game panel
+    game.classList.toggle('hidden');
 }
