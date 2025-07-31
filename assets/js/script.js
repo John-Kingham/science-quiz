299e992ab152e114bc81f45874e7fb9b15f4a80f
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-07-31 14:11:45 +0100

    Update js to run after DOM content loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-document.getElementById('instructions-button').addEventListener('click', (e) => {
-    const instructions = document.getElementById('instructions-text');
-    const welcome = document.getElementById('welcome-text');
-    const instructionsButton = document.getElementById('instructions-button');
+// Wait for the DOM to finish loading before running this script
+document.addEventListener('DOMContentLoaded', () => {
+
+    // add event listeners to each button
+    const instructionsButton = document.querySelector('#instructions-btn')
+    instructionsButton.addEventListener('click', showInstructions);
+});
+
+
+/**
+ * Show the instructions panel
+ */
+function showInstructions() {
+    const instructions = document.querySelector('#instructions-txt');
+    const welcome = document.querySelector('#welcome-txt');
+    const instructionsButton = document.querySelector('#instructions-btn');
 
     instructions.classList.toggle('hidden');
     welcome.classList.toggle('hidden');
     instructionsButton.classList.toggle('hidden');
-});
+}
