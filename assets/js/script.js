4670ea1f8a8f46e4c11e5a222a620e5faa6e218b
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-07-31 17:05:39 +0100

    Add JS functions to load questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ let instructionsBtn;
 let fiftyBtn;
 let askBtn;
 let phoneBtn;
+let question;
+let answers;
+let questionsAndAnswers;
+let currentQuestion;
 
 
 // Wait for the DOM to finish loading before running this script
@@ -15,11 +19,14 @@ document.addEventListener('DOMContentLoaded', () => {
     welcome = document.querySelector('#welcome');
     instructions = document.querySelector('#instructions');
     game = document.querySelector('#game');
+    question = document.querySelector('#question');
+    answers = document.querySelector('#answers');
     playBtn = document.querySelector('#play-btn');
     instructionsBtn = document.querySelector('#instructions-btn');
     fiftyBtn = document.querySelector('#fifty-btn');
     askBtn = document.querySelector('#ask-btn');
     phoneBtn = document.querySelector('#phone-btn');
+    loadQuestions();
 
     // add event listeners to buttons
     instructionsBtn.addEventListener('click', showInstructions);
@@ -41,6 +48,10 @@ function showInstructions() {
  * Show the game panel and start the game
  */
 function startGame() {
+
+    // show the first question
+    showNextQuestion();
+
     // hide non-game content
     welcome.classList.add('hidden');
     instructions.classList.add('hidden');
@@ -52,4 +63,47 @@ function startGame() {
     fiftyBtn.classList.remove('hidden');
     askBtn.classList.remove('hidden');
     phoneBtn.classList.remove('hidden');
+
+}
+
+
+/**
+ * Load questions (and answers)
+ */
+function loadQuestions() {
+    fetch('https://opentdb.com/api.php?amount=3&category=17&difficulty=easy&type=multiple')
+    .then(response => response.json())
+    .then(json => {
+        questionsAndAnswers = json.results;
+    });
+}
+
+
+/**
+ * Show the next question
+ */
+function showNextQuestion() {
+    currentQuestion = questionsAndAnswers.pop();
+    question.innerText = currentQuestion.question;
+    for (i = 0; i < 3; i++) {
+        answers.children[i].innerText = currentQuestion.incorrect_answers[i]; 
+    }
+    answers.children[3].innerText = currentQuestion.correct_answer;
+    console.log(currentQuestion.correct_answer);
+    shuffleQuestions(answers.children);
+}
+
+/**
+ * Shuffle an array-like object
+ * @param {*} arr 
+ * @returns undefined (mutates the passed-in array)
+ */
+function shuffleQuestions(arr) {
+    // Use the Fisher–Yates shuffle
+    for (let i = arr.length - 1; i > 0; i--) {
+         // pick random index from 0 to i
+        const j = Math.floor(Math.random() * (i + 1));
+        // swap the questions
+        [arr[i].innerText, arr[j].innerText] = [arr[j].innerText, arr[i].innerText]; 
+    }
 }
